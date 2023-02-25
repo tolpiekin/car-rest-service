42803c104667b417b5abef4172904c5b026005f8
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-02-25 19:57:07 +0200

    Cleaning up

--- src/main/java/ua/foxminded/volodymyrtolpiekin/carrestservice/repository/EVCarRepository.java
+++ src/main/java/ua/foxminded/volodymyrtolpiekin/carrestservice/repository/EVCarRepository.java
@@ -1,11 +1,8 @@
 package ua.foxminded.volodymyrtolpiekin.carrestservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ua.foxminded.volodymyrtolpiekin.carrestservice.models.CarModel;
 import ua.foxminded.volodymyrtolpiekin.carrestservice.models.EVCar;
 
-import java.awt.print.Pageable;
-import java.util.List;
 import java.util.Optional;
 
 public interface EVCarRepository extends JpaRepository<EVCar, Long> {
