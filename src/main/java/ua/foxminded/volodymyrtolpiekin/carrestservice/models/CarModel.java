6b68c13371b820b702ec55adf88a364cae0737ab
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-02-27 23:07:18 +0200

    Changed Hibernate Inheritance Mapping for CarModels to SINGLE_TABLE.

--- src/main/java/ua/foxminded/volodymyrtolpiekin/carrestservice/models/CarModel.java
+++ src/main/java/ua/foxminded/volodymyrtolpiekin/carrestservice/models/CarModel.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @ToString
 @RequiredArgsConstructor
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class CarModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
